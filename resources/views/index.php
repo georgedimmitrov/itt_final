9c34a9841341d05efbbe6617128ad01825b250f3
georgi <georgedimmitrov@gmail.com>
2016-04-05 15:00:37 +0300

    add registration and login forms

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,15 +1,38 @@
 <!doctype html>
 <html>
     <head>
-        <title>Time Tracker</title>
-        <link rel="stylesheet" href="css/normalize.css">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Undefined</title>
+        <meta name="description" content="">
+        <meta name="author" content="S.P. G.D.">
         
-        <link rel="stylesheet" href="css/style.css">
+        <!-- <link rel="stylesheet" href="css/normalize.css"> -->
         <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/jumbotron-narrow.css">
+        <link rel="stylesheet" href="css/app.css">
     </head>
-    <body ng-app="myApp">
+    <body ng-app="myApp" ng-cloak>
+        <div class="container">
+        <div class="header clearfix">
+            <nav>
+                <ul class="nav nav-pills pull-right nav-blue">
+                    <li role="presentation" class="active"><a href="/profile">My profile</a></li>
+                    <li role="presentation"><a href="/auth/logout">Logout</a></li>
+                </ul>
+            </nav>
+            <h3 class="text-muted"><a href="#/" style="color: #fff; text-decoration: none;">Undef</a></h3>
+        </div>
+        
         <div ng-view></div>
+        
+        <footer class="footer">
+            <p>&copy; Siyana and Georgi's masterpiece</p>
+        </footer>
+    </div>
+
+        
+        <!-- <div ng-view></div> -->
     </body>
 
     <!-- Application Dependencies -->

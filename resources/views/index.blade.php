3b07ee9ed48a752e4488631af466ca67146a23c8
Siyana Plachkova <siyanaplachkova@gmail.com>
2016-04-07 09:04:03 +0300

    Google login fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,27 @@
         <meta name="description" content="">
         <meta name="author" content="S.P. G.D.">
         <meta name="google-signin-scope" content="profile email">
-        <meta name="google-signin-client_id" content="301262529415-guutgbs0958kcmgdrtv13lj64a2h7m1o.apps.googleusercontent.com.apps.googleusercontent.com">
+        <meta name="google-signin-client_id" content="301262529415-hqa5h289utdjudkh9e8pcfrdinhkuuto.apps.googleusercontent.com">
         <script src="https://apis.google.com/js/platform.js" async defer></script>
+  </head>
+  <body>
+    <div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    <script>
+      function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      };
+    </script>
 
         <!-- <link rel="stylesheet" href="css/normalize.css"> -->
         <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
